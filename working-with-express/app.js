ad70dba094d5aad6e623f3f56468fc5a3cdad5e4
Valentyn Filippov <valentine@Valentyns-MacBook-Pro.local>
2020-09-03 19:33:55 +0300

    add html files

--- working-with-express/app.js
+++ working-with-express/app.js
@@ -1,17 +1,26 @@
+const path = require('path');
+
 const express = require('./node_modules/express');
 const bodyParses = require('./node_modules/body-parser');
 
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
+const rootDir = require('./util/path');
+
 const app = express();
 
 // Middleware
 app.use(bodyParses.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
 
-// Routes'
-app.use(adminRoutes);
+// Routes
+app.use('/admin', adminRoutes);
 app.use(shopRoutes);
+
+app.use((req, res, next) => {
+  res.sendFile(path.join(rootDir, 'views', '404.html'));
+});
 
 // "createServer" options
 const PORT = process.env.PORT || 80;
