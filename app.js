159ee7826ea55e1cd07b23ccd6fb8138de578959
Valentyn Filippov <valentine@Valentyns-MacBook-Pro.local>
2020-09-05 12:44:54 +0300

    added pug files

--- app.js
+++ app.js
@@ -5,17 +5,24 @@ const bodyParser = require('body-parser');
 
 const app = express();
 
+// setup default view engine configuration
+app.set('view engine', 'pug');
+// path to the files
+app.set('views', 'views');
+
 const adminData = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
+//
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
+// routes
 app.use('/admin', adminData.routes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-  res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
+  res.status(404).render('404');
 });
 
 app.listen(80);
--- routes/shop.js
+++ routes/shop.js
@@ -9,8 +9,9 @@ const adminData = require('./admin');
 const router = express.Router();
 
 router.get('/', (req, res, next) => {
-  console.log(adminData.products);
-  res.sendFile(path.join(rootDir, 'views', 'shop.html'));
+  // res.sendFile(path.join(rootDir, 'views', 'shop.html'));
+  const products = adminData.products;
+  res.render('shop', { prods: products, docTitle: 'Shop Pug' });
 });
 
 module.exports = router;
