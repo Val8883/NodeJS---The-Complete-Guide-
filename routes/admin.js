3dcea210e3bfea7580251d761a3f216b11319025
Valentyn Filippov <valentine@Valentyns-MacBook-Pro.local>
2020-09-05 11:21:00 +0300

    fix files

--- routes/admin.js
+++ routes/admin.js
@@ -6,8 +6,6 @@ const rootDir = require('../util/path');
 
 const router = express.Router();
 
-const products = [];
-
 // /admin/add-product => GET
 router.get('/add-product', (req, res, next) => {
   res.sendFile(path.join(rootDir, 'views', 'add-product.html'));
@@ -15,9 +13,8 @@ router.get('/add-product', (req, res, next) => {
 
 // /admin/add-product => POST
 router.post('/add-product', (req, res, next) => {
-  products.push({ title: req.body.title });
+  console.log(req.body.title);
   res.redirect('/');
 });
 
 exports.routes = router;
-exports.products = products;
